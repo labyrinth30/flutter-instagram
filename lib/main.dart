43776c2bc84f8ba4d467d66c62ff5eb13927c88f
younha00 <ayeon0@office.skhu.ac.kr>
2023-09-28 21:42:31 +0900

    웹, 안드로이드 파이어베이스 연결

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_instagram/responsive/responsive_layout_screen.dart';
 import 'package:flutter_instagram/screens/mobile_screen_layout.dart';
@@ -7,7 +8,19 @@ import 'package:flutter_instagram/utils/colors.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: "AIzaSyDCwyc4K5mUMZl7Q_GnP473Y3dFmKRPXXI",
+        appId: "1:15653772181:web:5afee694db648f0bb0f49c",
+        messagingSenderId: "15653772181",
+        projectId: "instagram-clone-96430",
+        storageBucket: "instagram-clone-96430.appspot.com",
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
   runApp(const MyApp());
 }
 
